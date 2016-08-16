4cb1768f5db16299af54dfea9b6a6bb85be2b580
Sergey Sokur <sergey_sokur@epam.com>
2016-08-16 13:20:57 +0300

    andr. fixed

--- Vpn.py
+++ Vpn.py
@@ -8,40 +8,42 @@ import time
 import os
 import json
 
-caps = DesiredCapabilities.INTERNETEXPLORER
-caps['ignoreProtectedModeSettings'] = True
-driver = webdriver.Ie(os.getcwd() + "\\IEDriverServer.exe", capabilities=caps)
-driver.implicitly_wait(10) # seconds
-wait = WebDriverWait(driver, 10)
 
+while True:
+	caps = DesiredCapabilities.INTERNETEXPLORER
+	caps['ignoreProtectedModeSettings'] = True
+	driver = webdriver.Ie(os.getcwd() + "\\IEDriverServer.exe", capabilities=caps)
+	driver.implicitly_wait(10) # seconds
+	wait = WebDriverWait(driver, 10)
 
-data = json.loads(open('data.json').read())
-driver.get(data["url"])
-wait.until(EC.title_contains("Check Point"))
-userName = driver.find_element_by_id("userName")
-userName.clear()
-userName.send_keys(data["login_data"]["login"])
-password = driver.find_element_by_id("passwordDisplayed")
-password.clear()
-password.send_keys(data["login_data"]["password"])
-# Login
-driver.find_element_by_id("LoginButton").click()
-driver.switch_to_window(driver.window_handles[1])
-connectionStatus = driver.find_element_by_id("displayStatus")
-# new version
-while(connectionStatus.text != "Подключено"):
-    time.sleep(1)
-print(connectionStatus.text)
-while (os.system("ping -n 1 " + data["hostname"]) == 0):
-    # print (data["hostname"] + 'is up!')
-    pass
-else:
-    # print (data["hostname"] + 'is down!')
-    #unexpected action with second window(popup)
-    driver.switch_to_window(driver.window_handles[0])
-    driver.close()
-    # driver.quit()
+	data = json.loads(open('data.json').read())
 
-# driver.find_element_by_id("overridelink").click()
+	driver.get(data["url"])
+	wait.until(EC.title_contains("Check Point"))
+	userName = driver.find_element_by_id("userName")
+	userName.clear()
+	userName.send_keys(data["login_data"]["login"])
+	password = driver.find_element_by_id("passwordDisplayed")
+	password.clear()
+	password.send_keys(data["login_data"]["password"])
+	# Login
+	driver.find_element_by_id("LoginButton").click()
+	# new version
+	errors = 0
+	while errors < 3:
+		while (os.system("ping -n 1 " + data["hostname"]) == 0):
+		    # print (data["hostname"] + 'is up!')
+		    time.sleep(10)
+		    errors = 0
+		else:
+		    # print (data["hostname"] + 'is down!')
+		    #unexpected action with second window(popup)
+		    # driver.switch_to_window(driver.window_handles[0])
+		    # driver.close()
+		    errors = errors + 1
+	else:
+	    driver.quit()
 
-#pyinstaller --onefile Vpn.py
+	# driver.find_element_by_id("overridelink").click()
+
+	#pyinstaller --onefile Vpn.py
